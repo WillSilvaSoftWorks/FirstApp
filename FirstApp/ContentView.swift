51cff04f8bdefe5b13d65b1541c6c0809a360836
WillSilvaSoftworks <WillSilvaSoftworks@gmail.com>
2023-08-09 23:35:35 -0400

    Initial Commit

---
+++ FirstApp/ContentView.swift
@@ -0,0 +1,33 @@
+
+
+import SwiftUI
+enum Emoji: String, CaseIterable {
+    case 🌵,🦈,👑,🪖
+}
+struct ContentView: View {
+    @State var selection: Emoji = .🪖
+    
+    var body: some View {
+        NavigationView {
+            VStack{
+                Text(selection.rawValue)
+                    .font(.system(size:150))
+                
+                Picker("Select Emoji", selection: $selection){
+                    ForEach(Emoji.allCases, id:\.self){ emoji in
+                        Text(emoji.rawValue)
+                    }
+                }.pickerStyle(.segmented)
+                
+            }
+            .padding()
+            .navigationTitle("Emoji Selection")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by William Silva on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
